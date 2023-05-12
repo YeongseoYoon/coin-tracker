ceb2b27d0a9dfb147adb3f26455c74426b310e49
Eagerbeaver <86523545+YeongseoYoon@users.noreply.github.com>
2023-05-12 11:16:08 +0900

    feat: 코인 상세 chart 옵션 추가

--- src/routes/Chart.tsx
+++ src/routes/Chart.tsx
@@ -1,5 +1,91 @@
-function Chart() {
-  return <h1>Chart</h1>;
+import { useQuery } from "@tanstack/react-query";
+import { fetchCoinHistory } from "../api";
+import ApexChart from "react-apexcharts";
+
+interface IHistorical {
+  time_open: string;
+  time_close: string;
+  open: number;
+  high: number;
+  low: number;
+  close: number;
+  volume: number;
+  market_cap: number;
+}
+
+interface ChartProps {
+  coinId: string;
+}
+
+function Chart({ coinId }: ChartProps) {
+  const { isLoading, data } = useQuery<IHistorical[]>(["ohlcv", coinId], () =>
+    fetchCoinHistory(coinId)
+  );
+  const exceptData = data ?? [];
+  const chartData = exceptData?.map((i) => {
+    return {
+      x: i.time_close,
+      y: [i.open, i.high, i.low, i.close],
+    };
+  });
+  return (
+    <div>
+      {isLoading ? (
+        "Loading chart..."
+      ) : (
+        <ApexChart
+          type="candlestick"
+          series={[
+            {
+              data: chartData,
+            },
+          ]}
+          options={{
+            annotations: {
+              points: [
+                {
+                  label: {
+                    style: {
+                      background: "#fff",
+                      color: "#180e0e !important",
+                      fontSize: "12px",
+                      fontWeight: 400,
+                    },
+                  },
+                },
+              ],
+            },
+            chart: {
+              height: 300,
+              width: 500,
+              toolbar: {
+                show: false,
+              },
+              background: "transparent",
+            },
+
+            title: {
+              text: "CandleStick Chart",
+              align: "left",
+            },
+            xaxis: {
+              type: "datetime",
+            },
+            yaxis: {
+              tooltip: {
+                enabled: true,
+              },
+            },
+            tooltip: {
+              y: {
+                formatter: (value) => `$${value.toFixed(2)}`,
+              },
+            },
+          }}
+        />
+      )}
+    </div>
+  );
 }
 
 export default Chart;
